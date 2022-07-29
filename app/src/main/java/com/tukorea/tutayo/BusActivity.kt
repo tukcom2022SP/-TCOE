d49960b718e7177e2304d2a3ab4214abc712b5aa
dami <dami0804@naver.com>
2022-07-29 15:59:40 +0900

    bustime1

--- app/src/main/java/com/tukorea/tutayo/BusActivity.kt
+++ app/src/main/java/com/tukorea/tutayo/BusActivity.kt
@@ -1,13 +1,11 @@
 package com.tukorea.tutayo
 
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
 import android.view.View
 import android.widget.*
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -20,23 +18,21 @@ class BusActivity : AppCompatActivity() {
     lateinit var OkBtn:Button
     lateinit var ResetBtn:Button
     lateinit var tx1:TextView
-
+    val Cal =Calendar.getInstance()
+    val dataFormat4 = SimpleDateFormat("HH:mm")
+    val dataFormat = DateTimeFormatter.ofPattern("HH:mm")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bus_activity)
-<<<<<<< HEAD
+
 
         var text: TextView = findViewById(R.id.nextBus_now)
         var current = System.currentTimeMillis()
-=======
+
         val nowbutton: Button = findViewById(R.id.nowtime)
         // val btn : Button = findViewById(R.id.ttime)
-        val text: TextView = findViewById(R.id.nextBus_now)
         var start: Long = 0
         var end: Long = 0
-
-        val current = System.currentTimeMillis()
->>>>>>> f553265abf1d4382f6f4c11ab04bca17f33db893
 
         linear = findViewById(R.id.linear)
         settingTime = findViewById(R.id.settingTime)
@@ -49,13 +45,12 @@ class BusActivity : AppCompatActivity() {
         tx1 = findViewById(R.id.tx1)
         var hour =Integer.parseInt(timePicker.hour.toString())
         var minute= Integer.parseInt(timePicker.minute.toString())
-        val dataFormat4 = SimpleDateFormat("HH:mm")
+
 
         tx1.text = "현재시각" +dataFormat4.format(current) + " 기준"
-//*********************************
+
 
         var tx4Cal = Calendar.getInstance()
-
 
 
         //세팅시간
@@ -63,22 +58,42 @@ class BusActivity : AppCompatActivity() {
         val datemin = Date(timePicker.minute.toLong())
         //var ttime :String = dataFormat4.format(date)
 
-        val Cal =Calendar.getInstance()
-        var settingtime: Date = dataFormat4.parse("15:00")//지정시간넣기
 
-        //버스시간 넣기
+      //  var settingtime: Date = dataFormat4.parse("$datehour:$datemin")//지정시간넣기
+        var settingtime: Date = dataFormat4.parse("13:00")//지정시간넣기
+
+        //버스시간 넣기(정왕역 출발)
         val cal1 = Calendar.getInstance()
-        val bus1: Date = dataFormat4.parse("13:05")
+        val bus_1: Date = dataFormat4.parse("8:41")
+        val bus_2: Date = dataFormat4.parse("8:59")
 
-        val cal2 = Calendar.getInstance()
-        val bus2: Date = dataFormat4.parse("13:05")
+       /* val bus_3: Date = dataFormat4.parse("9:5")
+        val bus_4: Date = dataFormat4.parse("9:15")
+        val bus_5: Date = dataFormat4.parse("9:20")
+        val bus_6: Date = dataFormat4.parse("9:35")
+        val bus_7: Date = dataFormat4.parse("9:50")
+
+        val bus_8: Date = dataFormat4.parse("10:5")
+        val bus_9: Date = dataFormat4.parse("10:20")
+        val bus_10: Date = dataFormat4.parse("10:35")
+        val bus_11: Date = dataFormat4.parse("10:55")
+
+        val bus_12: Date = dataFormat4.parse("11:10")
+        val bus_13: Date = dataFormat4.parse("11:30")
+        val bus_14: Date = dataFormat4.parse("11:50")*/
+var busdate = listOf(subCurrentTime(Cal,bus_1))
+var result = busdate.sortedByDescending {
+    LocalDate.parse(it,dataFormat)
+}
+
+        //버스시간 넣기(학교 출발)
+
         //기준시간과 가장 가까운 시간 찾기
 
         //시간 차이 출력하기
         Cal.time =settingtime //지정시간
+        cal1.time =bus_1 //버스시간 **최소차이의 버스대입
 
-        cal1.time =bus1 //버스시간
-        cal2.time =bus2
        // Cal.add(Cal.time.hours, -cal1.time.hours)
         //Cal.add(Cal.time.minutes, -cal1.time.minutes)
         Cal.add(Calendar.HOUR_OF_DAY, -cal1.time.hours)
@@ -87,35 +102,75 @@ class BusActivity : AppCompatActivity() {
         tx4Cal.add(Calendar.HOUR_OF_DAY, -cal1.time.hours)
         tx4Cal.add(Calendar.MINUTE, -cal1.time.minutes)
 
-        //*************************************
+
+
+
+/* var array = ArrayList<Int>()
+
+ //array.add(Integer.parseInt(subCurrentTime(Cal,bus_1)))
+/* array.add(sCurrentTime(Cal,bus_2).toInt())
+ array.add(subCurrentTime(Cal,bus_3).toInt())
+ array.add(subCurrentTime(Cal,bus_4).toInt())
+ array.add(subCurrentTime(Cal,bus_5).toInt())*/
+
+
+// array.sort();
+
+// var mini=array.first() //최소
+*/
+
+
+
+
+     //*************************************
 //OK버튼 눌렀을때 지정 시간 text가 뜨고 bus시간과 시간차가 보인다.
-        OkBtn.setOnClickListener {
-            tx1.text = timePicker.hour.toString() + " : " + timePicker.minute.toString() + " 기준"
-            // 시간간격 출력
-            text.text = dataFormat4.format(Cal.time)}
+ OkBtn.setOnClickListener {
+   //  tx1.text = timePicker.hour.toString() + " : " + timePicker.minute.toString() + " 기준"
+     // 시간간격 출력
+   //  text.text = dataFormat4.format(Cal.time)
+     tx1.text =  "현재시각" +dataFormat4.format(current) + " 기준"
+     //text.text = subCurrentTime(Cal,bus_1)
+    // text.text= dataFormat4.format(mini)
+ }
 
-        ResetBtn.setOnClickListener {
-            tx1.text =  "현재시각" +dataFormat4.format(current) + " 기준"
-            text.text = dataFormat4.format(tx4Cal.time)
-        }
+ ResetBtn.setOnClickListener {
+     tx1.text =  "현재시각" +dataFormat4.format(current) + " 기준"
+     text.text = dataFormat4.format(tx4Cal.time)
+ }
 
-            settingTime.setOnClickListener {
-                if (settingTime.isChecked) {
-                    linear.visibility = View.VISIBLE
-                } else {
-                    linear.visibility = View.GONE
-                }
+     settingTime.setOnClickListener {
+         if (settingTime.isChecked) {
+             linear.visibility = View.VISIBLE
+         } else {
+             linear.visibility = View.GONE
+         }
 
-            }
-            busschedule.setOnClickListener {
-                if (busschedule.isChecked) {
-                    scheduleimg.visibility = View.VISIBLE
-                } else {
-                    scheduleimg.visibility = View.GONE
-                }
+     }
+     busschedule.setOnClickListener {
+         if (busschedule.isChecked) {
+             scheduleimg.visibility = View.VISIBLE
+         } else {
+             scheduleimg.visibility = View.GONE
+         }
 
-            }
+     }
 
-    }}
+}fun subCurrentTime(Time: Calendar, BusTime:Date): String { //현재 Time에서 BusTime 빼기
+
+
+ val cal1 =Calendar.getInstance()
+ //BusTime= dataFormat4.parse("8:41")
+
+
+ cal1.time =BusTime
+
+ Cal.add(Calendar.HOUR_OF_DAY, -cal1.time.hours)
+ Cal.add(Calendar.MINUTE, -cal1.time.minutes)
+
+ return dataFormat4.format(Cal.time)
+}
+
+
+}
 
 
