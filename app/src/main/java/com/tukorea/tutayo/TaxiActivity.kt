5a8bfd96fd2b644c0c1148b71ef3c70ec6d250fe
jhkim <wellsy001012@gmail.com>
2022-08-01 14:54:35 +0900

    #6 임시 커밋 - 스피너 코드 작성중

--- app/src/main/java/com/tukorea/tutayo/TaxiActivity.kt
+++ app/src/main/java/com/tukorea/tutayo/TaxiActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import kotlinx.android.synthetic.main.taxi_new.*
 
 class TaxiActivity : AppCompatActivity() {
     private lateinit var fragmentManager : FragmentManager
