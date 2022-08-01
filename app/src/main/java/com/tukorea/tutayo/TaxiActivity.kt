fd14ad8e71e1e474e106ae51c6859a45cd4cef4d
jhkim <wellsy001012@gmail.com>
2022-08-01 16:24:40 +0900

    #6 새 글 작성 프래그먼트 호출

--- app/src/main/java/com/tukorea/tutayo/TaxiActivity.kt
+++ app/src/main/java/com/tukorea/tutayo/TaxiActivity.kt
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.taxi_activity.*
 
 class TaxiActivity : AppCompatActivity() {
     private lateinit var fragmentManager : FragmentManager
     private lateinit var JFragment : Fragment
     private lateinit var OFragment : Fragment
+    private lateinit var NewFragment : Fragment
     private lateinit var transaction : FragmentTransaction
     private lateinit var viewPagerFragment : ViewPagerFragment
 
@@ -21,12 +23,17 @@ class TaxiActivity : AppCompatActivity() {
         fragmentManager = supportFragmentManager
         JFragment = JeongwangFragment()
         OFragment = OidoFragment()
+        NewFragment = NewTaxiFragment()
         viewPagerFragment = ViewPagerFragment()
 
         transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.taxi_fragment_frame, viewPagerFragment).commitAllowingStateLoss()
 
-
+        writeNew.setOnClickListener {
+            //새 글 작성 버튼 클릭시 새 글 프래그먼트로 넘어감
+            transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.taxi_fragment_frame, NewFragment).commitAllowingStateLoss()
+        }
 
 
 
