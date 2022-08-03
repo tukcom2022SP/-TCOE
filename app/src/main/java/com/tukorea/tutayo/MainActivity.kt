e3b61e35c51ed16b1ace9f44da1ce9cfc2764917
nEunseo <nes020781@gmail.com>
2022-08-03 14:26:44 +0900

    #2 - 파일 수정 : MainActivity의 getLoginData() , 파일 삭제 : TaxiLoginActivity.kt , taxi_needlogin.xml , main_menulist_logout.xml

--- app/src/main/java/com/tukorea/tutayo/MainActivity.kt
+++ app/src/main/java/com/tukorea/tutayo/MainActivity.kt
@@ -89,7 +89,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
 
             R.id.menu_taxi -> {
-                //만약 택시 메뉴로 진입했을 때, 로그인이 되어 있지 않다면 로그인이 필요합니다 메뉴로 리턴하고
+                //만약 택시 메뉴로 진입했을 때, 로그인이 되어 있지 않다면 로그인이 필요합니다 토스트 메시지 띄움
                 //아니면 택시액티비티로 리턴 시키기
                 getLoginData()
 
@@ -160,8 +160,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun getLoginData(){
         UserApiClient.instance.me {user, error ->
             if (error != null) {
-                val intent = Intent(this, TaxiLoginActivity::class.java)
-                startActivity(intent)
+                Toast.makeText(this, "로그인 후에 이용해 주세요.", Toast.LENGTH_SHORT).show()
             }
             else if(user != null){
                 val intent = Intent(this, TaxiActivity::class.java)
