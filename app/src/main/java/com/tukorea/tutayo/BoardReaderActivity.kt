74dec35b48e9ae5277027b72fd09b20604fc2873
dami <dami0804@naver.com>
2022-08-08 16:21:38 +0900

    for pull

---
+++ app/src/main/java/com/tukorea/tutayo/BoardReaderActivity.kt
@@ -0,0 +1,11 @@
+package com.tukorea.tutayo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardReaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.boardreader_activity)
+    }
+}
---
+++ app/src/main/java/com/tukorea/tutayo/BoardWriterActivity.kt
@@ -0,0 +1,11 @@
+package com.tukorea.tutayo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardWriterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.boardwriter_activity)
+    }
+}
